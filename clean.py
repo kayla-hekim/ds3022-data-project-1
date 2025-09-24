58075f979779f9756d40ad2fa5d44d17ad2547d5
Kayla Kim <kayla.hekim@gmail.com>
2025-09-24 00:01:21 -0400

    did something to load, may not work, did something to clean too

--- clean.py
+++ clean.py
@@ -1,2 +1,80 @@
 import duckdb
 import logging
+
+
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='load.log'
+)
+logger = logging.getLogger(__name__)
+
+
+
+# remove duplicates
+def remove_duplicates(year_range=(2024,2025)):
+    con = None
+
+    try:
+        con = duckdb.connect(database='emissions.duckdb', read_only=False)
+        logger.info("Connected to DuckDB, ready to remove duplicates")
+
+        con.execute("SET schema='tlc';")
+
+        tables = []
+        for year in year_range:
+            
+
+        for each_table in tables:
+            con.execute(f"""
+                CREATE TABLE synthdata_clean AS 
+                SELECT DISTINCT * FROM synthdata;
+                        
+                DROP TABLE synthdata;
+                ALTER TABLE synthdata_clean RENAME TO synthdata;
+            """)
+
+    except Exception as e:
+        # print(f"An error occurred for yellow green taxi parquet loading: {e}")
+        logger.error(f"An error occurred yellow green taxi parquet loading: {e}")
+
+
+def zero_passengers_removed():
+    con = None
+
+
+def zero_miles_removed():
+    con = None
+
+
+def more_100mi_removed():
+    con = None
+
+
+def more_24hr_removed():
+    con = None
+
+
+def tests():
+    con = None
+
+
+# Call all methods from load.py here
+if __name__ == "__main__":
+    # remove duplicates
+    remove_duplicates()
+
+    # remove trips with 0 passengers
+    zero_passengers_removed()
+
+    # remove trips 0 miles in length
+    zero_miles_removed()
+
+    # remove trips greater than 100 miles in length
+    more_100mi_removed()
+
+    # remove trips greater than 24 hours in length
+    more_24hr_removed()
+
+    # include tests
+    tests()
--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import duckdb
 import os
 import logging
+import time
 
 
 logging.basicConfig(
@@ -12,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 
 # Loading yellow and green parquet files into emissions db
-def load_parquet_files():
+def load_parquet_files(years=range(2024, 2025)):
     con = None
 
     try:
@@ -20,67 +21,51 @@ def load_parquet_files():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance for yellow green taxi parquets")
 
-        con.execute("INSTALL httpfs; LOAD httpfs;")
-        con.execute("PRAGMA enable_object_cache=true;")
-        con.execute("CREATE SCHEMA IF NOT EXISTS tlc;")
-
-
+        # con.execute("INSTALL httpfs; LOAD httpfs;")
+        # con.execute("CREATE SCHEMA IF NOT EXISTS tlc;")
 
         # uploading and populating yellow 2024 tables from parquet
-        year = "2024"
-        color = "yellow"
-        for month in range(1,13):
-            input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_2024-{month:02d}.parquet"
-            logger.info(f"working on {input_file} now...")
-
-            table_name = f"{color}_{year}_{month:02d}"
-            
-            con.execute(f"""
-                -- SQL goes here
-                DROP TABLE IF EXISTS {table_name};
-                CREATE TABLE {table_name}
-                    AS
-                SELECT * FROM read_parquet('{input_file}', union_by_name=true);
-            """)
-            logger.info(f"Dropped if exists and created table {table_name} in emissions db")
-
+        # year = "2024"
         
+        for year in years:
+            color = "yellow"
+            for month in range(1,13):
+                input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02d}.parquet"
+                logger.info(f"working on {input_file} now...")
 
-        # uploading and populating green 2024 tables from parquet
-        color = "green"
-        for month in range(1,13):
-            input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_2024-{month:02d}.parquet"
-            logger.info(f"working on {input_file} now...")
-
-            table_name = f"{color}_{year}_{month:02d}"
+                table_name = f"{color}_{year}_{month:02d}"
+                
+                con.execute(f"""
+                    -- SQL goes here
+                    DROP TABLE IF EXISTS {table_name};
+                    CREATE TABLE {table_name}
+                        AS
+                    SELECT * FROM read_parquet('{input_file}', union_by_name=true);
+                """)
+                logger.info(f"Dropped if exists and created table {table_name} in emissions db")
+                time.sleep(60)
             
-            con.execute(f"""
-                -- SQL goes here
-                DROP TABLE IF EXISTS {table_name};
-                CREATE TABLE {table_name}
-                    AS
-                SELECT * FROM read_parquet('{input_file}', union_by_name=true);
-            """)
-            logger.info(f"Dropped if exists and created table {table_name} in emissions db")
 
+            # uploading and populating green 2024 tables from parquet
+            color = "green"
+            for month in range(1,13):
+                input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02d}.parquet"
+                logger.info(f"working on {input_file} now...")
 
-        
-        # in the future: aggregate all yellow green tables into 1 big one, but not for now
-        # con.execute("""
-        #     DROP TABLE IF EXISTS trips_2024;
-        #     CREATE TABLE trips_2024 AS
-        #     SELECT *, 'yellow' AS taxi_color, '2024-' || lpad(CAST(month AS VARCHAR), 2, '0') AS yyyymm
-        #     FROM (
-        #         -- UNION all yellow months
-        #         SELECT *, 1 AS month FROM yellow_2024_01
-        #         UNION ALL SELECT *, 2 FROM yellow_2024_02
-        #         ...
-        #         UNION ALL SELECT *, 12 FROM yellow_2024_12
-        #     )
-        #     UNION ALL
-        #     SELECT *, 'green' AS taxi_color, ...
-        #     -- same for green tables
-        # """)
+                table_name = f"{color}_{year}_{month:02d}"
+                
+                con.execute(f"""
+                    -- SQL goes here
+                    DROP TABLE IF EXISTS {table_name};
+                    CREATE TABLE {table_name}
+                        AS
+                    SELECT * FROM read_parquet('{input_file}', union_by_name=true);
+                """)
+                logger.info(f"Dropped if exists and created table {table_name} in emissions db")
+                time.sleep(60)
+
+        con.execute("VACUUM;")
+        logger.info("VACUUM completed")
 
     except Exception as e:
         # print(f"An error occurred for yellow green taxi parquet loading: {e}")
@@ -102,9 +87,8 @@ def load_vehicle_emissions_csv(file_name):
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance for vehicle emissions csv table")
 
-        con.execute("PRAGMA enable_object_cache=true;")
-        con.execute("CREATE SCHEMA IF NOT EXISTS tlc;")
-
+        # con.execute("CREATE SCHEMA IF NOT EXISTS tlc;")
+        # con.execute("SET schema='tlc';")        
 
         con.execute(f"""
             DROP TABLE IF EXISTS {table_name};
@@ -113,6 +97,7 @@ def load_vehicle_emissions_csv(file_name):
             SELECT * FROM read_csv_auto('{file_name}', HEADER=TRUE);
         """)
         logger.info(f"Loaded {file_name} into {table_name} table into emissions db")
+
         cnt = con.execute(f"SELECT COUNT(*) FROM {table_name}").fetchone()[0]
         logger.info(f"{table_name} has {cnt} rows")
 
@@ -126,57 +111,58 @@ def load_vehicle_emissions_csv(file_name):
 
 
 
-# performing basic summarizations in sql through duckdb connection 
-def basic_data_summarizations():
-    con = None
+# performing basic summarizations in sql through duckdb connection FOR 2024 FOR NOW
+# def basic_data_summarizations():
+#     con = None
 
-    try:
-        con = duckdb.connect(database='emissions.duckdb', read_only=False)
-        logger.info("Performing basic summarizations on green, yellow, and vehicle emissions tables in emissions db")
+#     try:
+#         con = duckdb.connect(database='emissions.duckdb', read_only=False)
+#         logger.info("Performing basic summarizations on green, yellow, and vehicle emissions tables in emissions db")
+#         con.execute("SET schema='tlc';")
 
-        # summing those in yellow and green tables
-        # yellow sum
-        tables = [f"yellow_2024_{m:02d}" for m in range(1, 13)]
-        union_sql_yellow = " UNION ALL ".join([f"SELECT COUNT(*) AS count FROM {t}" for t in tables])
-        query = f"SELECT SUM(count) FROM ({union_sql_yellow})"
-        total_yellow = con.execute(query).fetchone()[0]
-        logger.info("summing count from yellow tables")
+#         # summing those in yellow and green tables
+#         # yellow sum
+#         tables = [f"yellow_2024_{m:02d}" for m in range(1, 13)]
+#         union_sql_yellow = " UNION ALL ".join([f"SELECT COUNT(*) AS count FROM {t}" for t in tables])
+#         query = f"SELECT SUM(count) FROM ({union_sql_yellow})"
+#         total_yellow = con.execute(query).fetchone()[0]
+#         logger.info("summing count from yellow tables")
 
-         # green sum
-        tables = [f"green_2024_{m:02d}" for m in range(1, 13)]
-        union_sql_green = " UNION ALL ".join([f"SELECT COUNT(*) AS count FROM {t}" for t in tables])
-        query = f"SELECT SUM(count) FROM ({union_sql_green})"
-        total_green = con.execute(query).fetchone()[0]
-        logger.info("summing count from green tables")
+#          # green sum
+#         tables = [f"green_2024_{m:02d}" for m in range(1, 13)]
+#         union_sql_green = " UNION ALL ".join([f"SELECT COUNT(*) AS count FROM {t}" for t in tables])
+#         query = f"SELECT SUM(count) FROM ({union_sql_green})"
+#         total_green = con.execute(query).fetchone()[0]
+#         logger.info("summing count from green tables")
 
-        # logging sums and printing
-        print(f"Total yellow trips 2024: {total_yellow:,}")
-        print(f"Total green trips 2024: {total_green:,}")
-        logger.info(f"Total yellow trips 2024: {total_yellow:,}")
-        logger.info(f"Total green trips 2024: {total_green:,}")
-
-
-        # Average trip distance (sample from one month each color)
-        avg_yellow_dist = con.execute("SELECT AVG(trip_distance) FROM yellow_2024_01").fetchone()[0]
-        avg_green_dist = con.execute("SELECT AVG(trip_distance) FROM green_2024_01").fetchone()[0]
-        print(f"Average trip distance (yellow Jan 2024): {avg_yellow_dist:.2f} miles")
-        print(f"Average trip distance (green Jan 2024): {avg_green_dist:.2f} miles")
-        logger.info(f"Average trip distance (yellow Jan 2024): {avg_yellow_dist:.2f} miles")
-        logger.info(f"Average trip distance (green Jan 2024): {avg_green_dist:.2f} miles")
+#         # logging sums and printing
+#         print(f"Total yellow trips 2024: {total_yellow}")
+#         print(f"Total green trips 2024: {total_green}")
+#         logger.info(f"Total yellow trips 2024: {total_yellow}")
+#         logger.info(f"Total green trips 2024: {total_green}")
 
 
-        # Vehicle emissions table summary
-        cnt_emissions = con.execute("SELECT COUNT(*) FROM vehicle_emissions").fetchone()[0]
-        avg_emission = con.execute("SELECT AVG(co2_grams_per_mile) FROM vehicle_emissions").fetchone()[0]
-        print(f"vehicle_emissions rows: {cnt_emissions}, avg CO2 g/mi: {avg_emission:.2f}")
-        logger.info(f"vehicle_emissions rows: {cnt_emissions}, avg CO2 g/mi: {avg_emission:.2f}")
+#         # Average trip distance (sample from one month each color)
+#         avg_yellow_dist = con.execute("SELECT AVG(trip_distance) FROM yellow_2024_01").fetchone()[0]
+#         avg_green_dist = con.execute("SELECT AVG(trip_distance) FROM green_2024_01").fetchone()[0]
+#         print(f"Average trip distance (yellow Jan 2024): {avg_yellow_dist} miles")
+#         print(f"Average trip distance (green Jan 2024): {avg_green_dist} miles")
+#         logger.info(f"Average trip distance (yellow Jan 2024): {avg_yellow_dist} miles")
+#         logger.info(f"Average trip distance (green Jan 2024): {avg_green_dist} miles")
 
-    except Exception as e:
-        print(f"Error in summarization: {e}")
-        logger.error(f"Error in summarization: {e}")
-    finally:
-        if con:
-            con.close()
+
+#         # Vehicle emissions table summary
+#         cnt_emissions = con.execute("SELECT COUNT(*) FROM vehicle_emissions").fetchone()[0]
+#         avg_emission = con.execute("SELECT AVG(co2_grams_per_mile) FROM vehicle_emissions").fetchone()[0]
+#         print(f"vehicle_emissions rows: {cnt_emissions}, avg CO2 g/mi: {avg_emission}")
+#         logger.info(f"vehicle_emissions rows: {cnt_emissions}, avg CO2 g/mi: {avg_emission}")
+
+#     except Exception as e:
+#         print(f"Error in summarization: {e}")
+#         logger.error(f"Error in summarization: {e}")
+#     finally:
+#         if con:
+#             con.close()
 
 
 
@@ -187,4 +173,4 @@ if __name__ == "__main__":
     vehicle_emissions_csv = './data/vehicle_emissions.csv'
     load_vehicle_emissions_csv(vehicle_emissions_csv)
     # summarizations
-    basic_data_summarizations()
+    # basic_data_summarizations()
